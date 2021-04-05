fd9b1bc4142285125fef5fc7539712fef1b62c76
nhanncv <vinhnhan1999bc@gmail.com>
2021-04-05 21:23:01 +0700

    fix set headers

--- src/index.js
+++ src/index.js
@@ -18,6 +18,20 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5001');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-Requested-With,content-type'
+  );
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  next();
+});
 app.use('/public', express.static(path.join(__dirname, 'public')));
 app.use(configs.api.prefix, router);
 
